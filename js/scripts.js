cd1e7d1a0f1e95d11cc807eebbce4b377b0be335
Aaron Sarnat <accounts@pixelday.com>
2017-08-09 14:59:41 -0700

    adding publications page

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,6 @@
 // Initialize Syntax Highlighting
 hljs.initHighlightingOnLoad();
+
+// Add Selected class to nav link that matches page ID
+const pageID = document.body.getAttribute("data-page");
+document.querySelector(`nav li[data-page="${pageID}"]`).classList.add("header__content__nav__link--selected");
