d7099d89ff69da958048237d627539da945624fe
Aaron Sarnat <accounts@pixelday.com>
2017-08-11 00:55:34 -0700

    css refactor, added responsive footer

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,4 @@ hljs.initHighlightingOnLoad();
 
 // Add Selected class to nav link that matches page ID
 const pageID = document.body.getAttribute("data-page");
-document.querySelector(`nav li[data-page="${pageID}"]`).classList.add("header__content__nav__link--selected");
+document.querySelector(`nav li[data-page="${pageID}"]`).classList.add("nav__link--selected");
