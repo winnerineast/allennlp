dd17f8110f7beae1fc63df84d1da89e1993cd46b
Aaron Sarnat <accounts@pixelday.com>
2017-08-15 15:46:31 -0700

    responsive fixes

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,30 @@
 hljs.initHighlightingOnLoad();
 
 // Add Selected class to nav link that matches page ID
-const pageID = document.body.getAttribute("data-page");
-document.querySelector(`nav li[data-page="${pageID}"]`).classList.add("nav__link--selected");
+let pageId;
+
+function matchPage(e) {
+  if (e.getAttribute("data-page") === pageId) {
+    e.classList.add("nav__link--selected");
+  }
+}
+
+const navLinks = Array.prototype.slice.call(document.querySelectorAll("header nav li[data-page]"));
+if (document.body.hasAttribute("data-page")) {
+  pageId = document.body.getAttribute("data-page");
+  navLinks.forEach(matchPage);
+}
+
+// Toggle MOW menu
+let mowActive = false;
+const headerNav = document.querySelector("header nav");
+
+document.getElementById("header__mow-nav-trigger").onclick = function() {
+  if (!mowActive) {
+    headerNav.classList.add("nav--mow-active");
+    mowActive = true;
+  } else {
+    headerNav.classList.remove("nav--mow-active");
+    mowActive = false;
+  }
+}
