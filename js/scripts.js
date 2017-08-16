fa86a4068f4860deae59ab14fd9ec233de1c2d52
Aaron Sarnat <accounts@pixelday.com>
2017-08-15 23:31:15 -0700

    new tutorial md template

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,21 @@ if (document.body.hasAttribute("data-page")) {
   navLinks.forEach(matchPage);
 }
 
+// Add Selected class to nav link that matches Tutorial ID
+let tutId;
+
+function matchTut(e) {
+  if (e.getAttribute("data-tutorial-id") === tutId) {
+    e.classList.add("tutorial__nav--selected");
+  }
+}
+
+const tutNavLinks = Array.prototype.slice.call(document.querySelectorAll(".tutorial__nav li[data-tutorial-id]"));
+if (document.body.hasAttribute("data-tutorial-id")) {
+  tutId = document.body.getAttribute("data-tutorial-id");
+  tutNavLinks.forEach(matchTut);
+}
+
 // Toggle MOW menu
 let mowActive = false;
 const headerNav = document.querySelector("header nav");
@@ -29,3 +44,5 @@ document.getElementById("header__mow-nav-trigger").onclick = function() {
     mowActive = false;
   }
 }
+
+//window.location.pathname.replace(/^\/([^\/]*).*$/, '$1');
